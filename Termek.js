9ef8e2e8bce02d11cccbf6bf55732bd58fda0094
Nao <ktimeabarbara@gmail.com>
2025-05-21 11:10:49 +0200

    Termek.js, Termekek.js - update, logo hozzaadva

--- Termek.js
+++ Termek.js
@@ -1,5 +1,5 @@
 export default class Termek {
-    #adat = {};
+    #adat;
     #szElem;
     #index;
     constructor(adat, szElem, index) {
@@ -9,31 +9,30 @@ export default class Termek {
         this.megjelenit();
     }
     megjelenit() {
-        const html = `
-                    <div class="card m-2" style="width: 18rem;">
-                    <img src="${this.#adat.kep}" class="card-img-top" alt="${this.#adat.nev}" />
-                        <div class="card-body">
-                            <h5 class="card-title">${this.#adat.nev}</h5>
-                            <p class="fw-bold">${this.#adat.ar} Ft</p>
-                            <p class="card-text">${this.#adat.leiras}</p>
-                            <a href="#" class="btn btn-primary kosarba" id="kosarba${this.#index}">Kosárba</a>
-                        </div>
-                    </div>`
-        this.#szElem.insertAdjacentHTML("beforeend", html);
+        const termekKartya = document.createElement("div");
+        termekKartya.classList.add("termekKartya");
+        termekKartya.innerHTML = `
+            <div class="card m-2" style="width: 18rem;">
+                <img src="${this.#adat.kep}" class="card-img-top" alt="${this.#adat.nev}" />
+                <div class="card-body">
+                    <h5 class="card-title">${this.#adat.nev}</h5>
+                    <p class="fw-bold">${this.#adat.ar} Ft</p>
+                    <p class="card-text">${this.#adat.leiras}</p>
+                    <button id="kosarba${this.#index}">Kosárba</button>
+                </div>
+            </div>
+        `;
+        this.#szElem.appendChild(termekKartya);
         this.#kosarbaGombEsemeny();
     }
     #kosarbaGombEsemeny() {
-        const kosarGomb = document.getElementById("kosarba${this.#index}");
-        if (kosarGomb) {
+        const kosarGomb = document.getElementById(`kosarba${this.#index}`);
         kosarGomb.addEventListener("click", () => {
-            console.log(`${this.#adat.nev} hozzáadta a kosarához!:)`);
+            console.log(`${this.#adat.nev} hozzáadta a kosarához! :)`);
             const esemeny = new CustomEvent("kosarba", {
                 detail: this.#adat,
             });
-            window.dispatchEvent(esemeny);  
+            window.dispatchEvent(esemeny);
         });
-        } else {
-            console.error(`Nem található gomb: kosarba${this.#index}`);
-        }
     }
 }
---
+++ Termekek.js
@@ -0,0 +1,24 @@
+import Termek from "./Termek.js";
+
+export default class Termekek {
+    #termekekLista = [];
+    #szElem;
+    constructor(termekekLista, szElem) {
+        this.#termekekLista = termekekLista;
+        this.#szElem = szElem;
+        this.megjelenit();
+        this.kosarbaEsemeny();
+    }
+    megjelenit() {
+        this.#szElem.innerHTML = "";
+        this.#termekekLista.forEach((adat, index) => {
+            new Termek(adat, this.#szElem, index);
+        });
+    }
+    kosarbaEsemeny() {
+        window.addEventListener("kosarba", (event) => {
+            const termek = event.detail;
+            console.log("Kosárba került:", termek);
+        });
+    }
+}
